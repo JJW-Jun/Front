399ab7c64c8c2921622f5e07e407993792a7d07d
jwoojun <jjwjun10@gmail.com>
2021-07-11 08:00:36 +0900

    react_basic

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import './App.css';
+import { Jumbotron, Container } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <Jumbotron fluid className="background">
+        <Container>
+          <h1>ZeroShop</h1>
+          <p>
+            We get back as much as we think of the earth. To use nature cleanly, to pass on beautiful nature to our offspring is a duty that we must bear. You can protect this from your little practice. Even if you don't use our products, please do it for nature.
+
+    </p>
+        </Container>
+      </Jumbotron>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">aasdf
+        <img className="product1" alt="ban" src='img/product/ban.jpg' width="100%"></img>
+            <h4>상품명</h4>
+            <p>상품설명 & 가격</p>
+            <img className="product1" alt="ban" src='img/product/ban.jpg' width="100%"></img>
+            <h4>상품명</h4>
+            <p>상품설명 & 가격</p>
+            <img className="product1" alt="ban" src='img/product/ban.jpg' width="100%"></img>
+            <h4>상품명</h4>
+            <p>상품설명 & 가격</p>
+          </div>
+
+          <div className="col-md-4">aasdf
+        <img className="product1" alt="ban" src='img/product/ban.jpg' width="100%"></img>
+            <h4>상품명</h4>
+            <p>상품설명 & 가격</p>
+            <img className="product1" alt="ban" src='img/product/ban.jpg' width="100%"></img>
+            <h4>상품명</h4>
+            <p>상품설명 & 가격</p>
+            <img className="product1" alt="ban" src='img/product/ban.jpg' width="100%"></img>
+            <h4>상품명</h4>
+            <p>상품설명 & 가격</p>
+          </div>
+          <div className="col-md-4">aasdf
+        <img className="product1" alt="ban" src='img/product/ban.jpg' width="100%"></img>
+            <h4>상품명</h4>
+            <p>상품설명 & 가격</p>
+            <img className="product1" alt="ban" src='img/product/ban.jpg' width="100%"></img>
+            <h4>상품명</h4>
+            <p>상품설명 & 가격</p>
+            <img className="product1" alt="ban" src='img/product/ban.jpg' width="100%"></img>
+            <h4>상품명</h4>
+            <p>상품설명 & 가격</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
